63ef11b94de6d2e4de8d1bd10d9fa61340ad7860
unknown <vincent_field@foxmail.com>
2019-05-14 12:29:47 +0800

    modify to procedure

--- sql/table_space_shower.sql
+++ sql/table_space_shower.sql
@@ -1,3 +1,11 @@
-select table_name, (data_length/1024/1024) as data_mb , (index_length/1024/1024)   
-as index_mb, ((data_length+index_length)/1024/1024) as all_mb, table_rows,(index_length * 100 /(index_length + data_length)) index_percent
-from information_schema.tables where table_schema = 'demo' order by index_percent desc;
+DELIMITER $$
+
+CREATE PROCEDURE `table_space_show`(IN dbname VARCHAR(64))
+BEGIN
+    DECLARE dbname VARCHAR(200) DEFAULT DATABASE();
+    SELECT `table_name`, (`data_length`/1024/1024) AS data_mb , (`index_length`/1024/1024)   
+AS index_mb, ((`data_length`+`index_length`)/1024/1024) AS all_mb, `table_rows`,(`index_length` * 100 /(`index_length` + `data_length`)) AS index_percent
+FROM `information_schema`.`tables` WHERE `table_schema` = dbname ORDER BY `index_percent` DESC;
+END$$
+
+DELIMITER ;
